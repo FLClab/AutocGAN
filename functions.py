02e8eeddf20bfa24cbe09d9236923f84a1d4b09e
catherinebouchard22 <46450217+catherinebouchard22@users.noreply.github.com>
2021-12-16 10:42:31 -0500

    Fixed batch size issue in functions.py

--- functions.py
+++ functions.py
@@ -141,6 +141,7 @@ def train_shared_cgan(
     return dynamic_reset
 
 
+
 def train_shared(
     args,
     gen_net: nn.Module,
@@ -460,7 +461,7 @@ def get_is(args, gen_net: nn.Module, num_img):
     return mean
 
 
-def validate(args, fixed_z, fid_stat, gen_net: nn.Module, writer_dict, clean_dir=True):
+def validate_gan(args, fixed_z, fid_stat, gen_net: nn.Module, writer_dict, clean_dir=True):
     writer = writer_dict["writer"]
     global_steps = writer_dict["valid_global_steps"]
 
@@ -496,6 +497,75 @@ def validate(args, fixed_z, fid_stat, gen_net: nn.Module, writer_dict, clean_dir
         for img_idx, img in enumerate(gen_imgs):
             file_name = os.path.join(
                 fid_buffer_dir, f"iter{iter_idx}_b{img_idx}.png")
+            imsave(file_name, img)
+        img_list.extend(list(gen_imgs))
+
+    # get inception score
+    logger.info("=> calculate inception score")
+    mean, std = get_inception_score(img_list)
+    print(f"Inception score: {mean}")
+
+    # get fid score
+    logger.info("=> calculate fid score")
+    fid_score = calculate_fid_given_paths(
+        [fid_buffer_dir, fid_stat], inception_path=None
+    )
+    print(f"FID score: {fid_score}")
+
+    if clean_dir:
+        os.system("rm -r {}".format(fid_buffer_dir))
+    else:
+        logger.info(f"=> sampled images are saved to {fid_buffer_dir}")
+
+    writer.add_image("sampled_images", img_grid, global_steps)
+    writer.add_scalar("Inception_score/mean", mean, global_steps)
+    writer.add_scalar("Inception_score/std", std, global_steps)
+    writer.add_scalar("FID_score", fid_score, global_steps)
+
+    writer_dict["valid_global_steps"] = global_steps + 1
+
+    return mean, fid_score
+
+def validate(args, fixed_z, fid_stat, gen_net: nn.Module, writer_dict, clean_dir=True):
+    writer = writer_dict["writer"]
+    global_steps = writer_dict["valid_global_steps"]
+
+    # eval mode
+    gen_net = gen_net.eval()
+
+    # generate images
+    fixed_labels = torch.cuda.LongTensor(np.random.randint(0, args.n_classes, (25)))
+    sample_imgs = gen_net(fixed_z, fixed_labels)
+    img_grid = make_grid(sample_imgs, nrow=5, normalize=True, scale_each=True)
+
+    # get fid and inception score
+    fid_buffer_dir = os.path.join(
+        args.path_helper["sample_path"], "fid_buffer")
+    os.makedirs(fid_buffer_dir, exist_ok=True)
+
+    eval_iter = args.num_eval_imgs // args.eval_batch_size
+    img_list = list()
+    for iter_idx in tqdm(range(eval_iter), desc="sample images"):
+        z = torch.cuda.FloatTensor(
+            np.random.normal(0, 1, (args.eval_batch_size, args.latent_dim))
+        )
+        labels = torch.cuda.LongTensor(
+            np.random.randint(0, args.n_classes, (args.eval_batch_size))
+        )
+
+        # Generate a batch of images
+        gen_imgs = (
+            gen_net(z, labels)
+            .mul_(127.5)
+            .add_(127.5)
+            .clamp_(0.0, 255.0)
+            .permute(0, 2, 3, 1)
+            .to("cpu", torch.uint8)
+            .numpy()
+        )
+        for img_idx, img in enumerate(gen_imgs):
+            file_name = os.path.join(
+                fid_buffer_dir, f"iter{iter_idx}_b{img_idx}_label{labels[img_idx]}.png")
             imsave(file_name, img)
         img_list.extend(list(gen_imgs))
 
