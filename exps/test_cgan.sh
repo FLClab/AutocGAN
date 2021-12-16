53d35a5810c8205020cc05da09bcf0138d07df07
catherinebouchard22 <46450217+catherinebouchard22@users.noreply.github.com>
2021-12-16 13:04:12 -0500

    Added exps/test_cgan.sh

---
+++ exps/test_cgan.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+python test.py \
+-gen_bs 128 \
+-dis_bs 128 \
+--dataset cifar10 \
+--bottom_width 4 \
+--img_size 32 \
+--max_iter 50000 \
+--gen_model shared_cgan \
+--dis_model shared_cgan \
+--latent_dim 128 \
+--gf_dim 256 \
+--df_dim 128 \
+--g_spectral_norm False \
+--d_spectral_norm True \
+--g_lr 0.0002 \
+--d_lr 0.0002 \
+--beta1 0.0 \
+--beta2 0.9 \
+--init_type xavier_uniform \
+--n_critic 5 \
+--val_freq 20 \
+--arch 0 0 1 1 1 0 1 1 0 0 0 1 1 3 \
+--load_path 'logs/derive_cgan_p90_BS4_2021_12_16_15_12_49/Model/checkpoint_best.pth' \
+--n_classes 10 \
+--exp_name derive_cgan_p90_BS4
